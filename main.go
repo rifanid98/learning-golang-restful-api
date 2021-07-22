29568f56311751e3b735d229169d44687ff55f98
rifanid98 <adninsijawa.office@gmail.com>
2021-07-22 17:10:00 +0700

    add correlation id middleware

--- main.go
+++ main.go
@@ -10,15 +10,17 @@ import (
 	"github.com/ilyakaznacheev/cleanenv"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+	"github.com/labstack/gommon/random"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var (
-	c    *mongo.Client
-	db   *mongo.Database
-	coll *mongo.Collection
-	cfg  config.Properties
+	C             *mongo.Client
+	db            *mongo.Database
+	coll          *mongo.Collection
+	cfg           config.Properties
+	CorrelationId = "X-Correlation-Id"
 )
 
 func init() {
@@ -28,19 +30,39 @@ func init() {
 
 	mongoURL := fmt.Sprintf("mongodb://%s:%s", cfg.DBHost, cfg.DBPort)
 
-	c, err := mongo.Connect(context.Background(), options.Client().ApplyURI(mongoURL))
+	C, err := mongo.Connect(context.Background(), options.Client().ApplyURI(mongoURL))
 	if err != nil {
 		log.Fatalf("Unable to connect to database : %v", err)
 	}
 
-	db = c.Database(cfg.DBName)
+	db = C.Database(cfg.DBName)
 	coll = db.Collection(cfg.DBCollection)
+}
+
+func addCorrelationId(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		// generate correlation id
+		var newId string
+
+		id := c.Request().Header.Get(CorrelationId)
+		if id == "" {
+			// generate random number
+			newId = random.String(12)
+		} else {
+			newId = id
+		}
+
+		c.Request().Header.Set(CorrelationId, newId)
+		c.Response().Header().Set(CorrelationId, newId)
+		return next(c)
+	}
 }
 
 func main() {
 	e := echo.New()
 
 	e.Pre(middleware.RemoveTrailingSlash())
+	e.Pre(addCorrelationId)
 
 	h := &products.ProductsHandler{Coll: coll}
 	e.POST("/products", h.CreateProducts, middleware.BodyLimit("1M"))
